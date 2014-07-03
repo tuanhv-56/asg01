6155e965992ae917e9fbb799f55ee4f1309c09f4
Hong Duong Vu <hongduongvu93@gmail.com>
2014-07-03 22:28:01 +0700

    Bai tap java

---
+++ Beer.java
@@ -0,0 +1,13 @@
+public class Beer {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ha Van Tuan\t" + "11020361\t" + "K56CC"
+				+ "tuanhv-56" + "tuanhv_56@vnu.edu.vn\t");
+		for (int i = 9; i > 0; i--) {
+			System.out.println(+i + " bottles of beer on the wall,\t" + i
+				+ " bottles of beer.\n" + "Take one down , pass it around");
+
+		}
+	}
+}
